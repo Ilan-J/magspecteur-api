0aead90518da723e363c84b427466c6b57cff120
Ilan-J <95917891+Ilan-J@users.noreply.github.com>
2024-10-08 15:03:42 +0200

    Initial commit

---
+++ src/test/java/com/magspecteur/api/MagspecteurApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magspecteur.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagspecteurApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
